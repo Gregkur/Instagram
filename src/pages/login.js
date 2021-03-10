c66d7cc27d143fd02b666041ffb1165eb9b26d17
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-03-10 19:49:32 +0100

    added and configured tailwind

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,5 +18,9 @@ export default function Login() {
   useEffect(() => {
     document.title = "Login - Instagram";
   }, []);
-  return <p>I am the login page</p>;
+  return (
+    <div className=" container flex mx-auto max-w-screen-md items-center h-screen">
+      <p>HELLO</p>
+    </div>
+  );
 }

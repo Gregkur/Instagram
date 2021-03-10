25354c20c17cb9f8baea9a5b82289d892e583414
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-03-10 21:15:41 +0100

    added the login form without the backend

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,29 +19,50 @@ export default function Login() {
     document.title = "Login - Instagram";
   }, []);
   return (
-    <div className=" container flex mx-auto max-w-screen-md items-center h-screen">
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
       <div className="flex w-3/5">
-        <img src="images/iphone-with-profile.jpg" alt="iPhone with profile" />
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iPhone with Instagram app"
+        />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img
-            src="images/logo.png"
-            alt="instagram"
-            className="mt-2 w-6/12 mb-4"
-          />
-        </h1>
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
 
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-
-        <form onSubmit={handleLogin} method="POST">
-          <imput
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Enter your email address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary "
-          />
-        </form>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              //   onChange={({ target }) => setEmailAddress(target.value)}
+              //   value={emailAddress}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              //   onChange={({ target }) => setPassword(target.value)}
+              //   value={password}
+            />
+            <button
+              //   disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+            ${isInvalid && "opacity-50"}`}>
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
